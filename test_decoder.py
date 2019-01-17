74aa9ef5bb76bc1fc0850fd15cff858d9c4f9f92
mdterrenal <mariaterrenal91@gmail.com>
2019-01-16 23:28:57 -0800

    Create skeleton classes for unit testing

---
+++ test_decoder.py
@@ -0,0 +1,11 @@
+"""A module to test the methods in the Decoder class."""
+
+import unittest
+
+class TestDecoder(unittest.TestCase):
+    """A class to test the functionality of the Decoder class."""
+
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_encoder.py
@@ -0,0 +1,11 @@
+"""A module to test the methods in the Encoder class."""
+
+import unittest
+
+class TestEncoder(unittest.TestCase):
+    """A class to test the functionality of the Encoder class."""
+
+    pass
+
+if __name__ == '__main__':
+    unittest.main()

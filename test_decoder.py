26fb0c25b9bf89e0e14eacb2fbb494633f0b9b67
mdterrenal <mariaterrenal91@gmail.com>
2019-01-17 23:18:37 -0800

    Create method skeletons for testing

--- test_decoder.py
+++ test_decoder.py
@@ -5,7 +5,24 @@ import unittest
 class TestDecoder(unittest.TestCase):
     """A class to test the functionality of the Decoder class."""
 
-    pass
+    def test_empty_string():
+        decoder = Decoder('')
+        self.assertEqual(decoder.decode_message(3), '')
+        self.assertEqual(decoder.decode_message(14), '')
+        self.assertEqual(decoder.decode_message(26), '')
+
+    def test_all_caps():
+        decoder = Decoder()
+
+    def test_all_lower():
+        decoder = Decoder()
+
+    def test_mixed_case():
+        decoder = Decoder()
+
+    def test_mixed_chars():
+        decoder = Decoder()
+
 
 if __name__ == '__main__':
     unittest.main()
--- test_encoder.py
+++ test_encoder.py
@@ -5,7 +5,24 @@ import unittest
 class TestEncoder(unittest.TestCase):
     """A class to test the functionality of the Encoder class."""
 
-    pass
+    def test_empty_string():
+        encoder = Encoder('')
+        self.assertEqual(encoder.encode_message(3), '')
+        self.assertEqual(encoder.encode_message(14), '')
+        self.assertEqual(encoder.encode_message(26), '')
+
+    def test_all_caps():
+        encoder = Encoder()
+
+    def test_all_lower():
+        encoder = Encoder()
+
+    def test_mixed_case():
+        encoder = Encoder()
+
+    def test_mixed_chars():
+        encoder = Encoder()
+
 
 if __name__ == '__main__':
     unittest.main()

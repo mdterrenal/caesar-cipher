2c84b4b5e9443844317eead9346b3a6ad879b16a
mdterrenal <mariaterrenal91@gmail.com>
2019-01-17 23:32:34 -0800

    Create simple tests for Encoder class

--- test_decoder.py
+++ test_decoder.py
@@ -1,26 +1,28 @@
 """A module to test the methods in the Decoder class."""
 
 import unittest
+from decoder import Decoder
+
 
 class TestDecoder(unittest.TestCase):
     """A class to test the functionality of the Decoder class."""
 
-    def test_empty_string():
+    def test_empty_string(self):
         decoder = Decoder('')
         self.assertEqual(decoder.decode_message(3), '')
         self.assertEqual(decoder.decode_message(14), '')
         self.assertEqual(decoder.decode_message(26), '')
 
-    def test_all_caps():
+    def test_all_caps(self):
         decoder = Decoder()
 
-    def test_all_lower():
+    def test_all_lower(self):
         decoder = Decoder()
 
-    def test_mixed_case():
+    def test_mixed_case(self):
         decoder = Decoder()
 
-    def test_mixed_chars():
+    def test_mixed_chars(self):
         decoder = Decoder()
 
 
--- test_encoder.py
+++ test_encoder.py
@@ -1,28 +1,33 @@
 """A module to test the methods in the Encoder class."""
 
 import unittest
+from encoder import Encoder
+
 
 class TestEncoder(unittest.TestCase):
     """A class to test the functionality of the Encoder class."""
 
-    def test_empty_string():
+    def test_empty_string(self):
         encoder = Encoder('')
         self.assertEqual(encoder.encode_message(3), '')
         self.assertEqual(encoder.encode_message(14), '')
         self.assertEqual(encoder.encode_message(26), '')
 
-    def test_all_caps():
-        encoder = Encoder()
+    def test_all_caps(self):
+        encoder = Encoder('HELLO')
+        self.assertEqual(encoder.encode_message(2), 'JGNNQ')
 
-    def test_all_lower():
-        encoder = Encoder()
+    def test_all_lower(self):
+        encoder = Encoder('hello')
+        self.assertEqual(encoder.encode_message(2), 'JGNNQ')
 
-    def test_mixed_case():
-        encoder = Encoder()
+    def test_mixed_case(self):
+        encoder = Encoder('hElLo')
+        self.assertEqual(encoder.encode_message(2), 'JGNNQ')
 
-    def test_mixed_chars():
-        encoder = Encoder()
-
+    def test_mixed_chars(self):
+        encoder = Encoder('he ll o!')
+        self.assertEqual(encoder.encode_message(2), 'JG NN Q!')
 
 if __name__ == '__main__':
     unittest.main()
